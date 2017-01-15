f9b8b043febdacba61ab6dc7145c77d4b178dcdc
applqpak <applqpak@gmail.com>
2017-01-15 16:53:49 +1100

    Create Controller.php

---
+++ src/Auth-FFDB/utils/Controller.php
@@ -0,0 +1,37 @@
+<?php
+
+  include 'Model.php';
+
+  class Controller
+  {
+      private $model;
+
+      public function __construct()
+      {
+          $this->model = new Model();
+      }
+
+      public function login($u, $p)
+      {
+          $p = base64_encode($p);
+          return $this->model->login($u, $p);
+      }
+
+      public function register($u, $p)
+      {
+          $p = password_hash(
+              base64_encode($p),
+              PASSWORD_DEFAULT,
+              [
+                  'cost' => 9
+              ]
+          );
+          return $this->model->register($u, $p);
+      }
+
+      public function fetch($u, $p)
+      {
+          $p = base64_encode($p);
+          return $this->model->fetch($u, $p);
+      }
+  }
